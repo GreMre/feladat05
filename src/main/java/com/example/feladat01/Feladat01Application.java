0bdbef9f0cd97124e1052d1baa9900119c2dfeeb
GreMre <gretikemrena@gmail.com>
2023-11-24 18:50:57 +0100

    Create Feladat01Application.java

---
+++ src/main/java/com/example/feladat01/Feladat01Application.java
@@ -0,0 +1,13 @@
+package com.example.feladat01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Feladat01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Feladat01Application.class, args);
+    }
+
+}
