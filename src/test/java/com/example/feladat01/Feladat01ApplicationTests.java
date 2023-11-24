ec7853c8d451e35e3de25962311b67e6a58ae73a
GreMre <gretikemrena@gmail.com>
2023-11-24 18:49:02 +0100

    Create Feladat01ApplicationTests.java

---
+++ src/test/java/com/example/feladat01/Feladat01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feladat01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Feladat01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
